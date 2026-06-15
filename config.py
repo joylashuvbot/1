82b2177d334c2210b980199eefbef17138310644
joylashuvbot <joylashuvbot@gmail.com>
2026-06-15 23:20:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8647959441:AAFZ-8CaYu-gH0lNoWa9w_TVx4eXyrSHqrk")
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:IAQKJZcjkVfuycAZwaeGmJimFzYOyNHF@thomas.proxy.rlwy.net:44630/railway")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:vlePdKdpwQhQyVTMjOsdMLfluFGGHaJM@thomas.proxy.rlwy.net:43373/railway")
 WEBAPP_URL = os.getenv("WEBAPP_URL", "https://asliddinx278-ops.github.io/88/")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "LoveBot!2026!StrongPass")
 
